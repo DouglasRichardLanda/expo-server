ae2c7d31237b4b1d755d07c83b665342b54d1ed3
Douglas Richard Landa <douglasrichardlanda@gmail.com>
2025-05-16 22:41:02 +0200

    adjustments

--- src/endpoints/calendar-controller.ts
+++ src/endpoints/calendar-controller.ts
@@ -11,42 +11,31 @@ class CalendarController {
   async calendar_report_week(req: REQ, res: RES) {
     const {current, id} = req.query; // current is the date, we receive it from user because users may have different time zones. ID for future DB
     const today = new Date(current as string)
-
     const next14Days = days_advanced_counter(today, 14)
-
     let fullDayResults: string[] = []
     let firstHalfResults: string[] = []
     let secondHalfResults: string[] = []
-
     next14Days.forEach((unit: Date)=> matrix_distributor(unit, fullDayResults, firstHalfResults, secondHalfResults, HUMAN_ENTITY1))
-
     res.status(200).json({firstHalfResults, secondHalfResults, fullDayResults})
   }
 
   async calendar_report_month(req: REQ, res: RES) {
     const {current, id} = req.query; // current is the date, we receive it from user because users may have different time zones. ID for future DB
     const today = new Date(current as string)
-
     const next56Days = days_advanced_counter(today, 56)
-
     let fullDayResults: string[] = []
     let firstHalfResults: string[] = []
     let secondHalfResults: string[] = []
-
     next56Days.forEach((unit: Date)=> matrix_distributor(unit, fullDayResults, firstHalfResults, secondHalfResults, HUMAN_ENTITY1))
-
     res.status(200).json({firstHalfResults, secondHalfResults, fullDayResults})
   }
 
   async any_date_report(req: REQ, res: RES) {
     const {date, id} = req.query; // current is the date, we receive it from user because users may have different time zones. ID for future DB
-
     const specificdate = new Date(date as string)
-
     let fullDayResult: string = comperer(COMPARISON_MATRIX.get(HUMAN_ENTITY1.luckynumber) as number[][], date_number(specificdate))
     let firstHalfResult: string = comperer(COMPARISON_MATRIX.get(HUMAN_ENTITY1.namenumber) as number[][], digit_normaliser(specificdate.getDate()))
     let secondHalfResult: string = comperer(COMPARISON_MATRIX.get(HUMAN_ENTITY1.birthdaynumber) as number[][], date_number(specificdate))
-
     res.status(200).json({firstHalfResult, secondHalfResult, fullDayResult})
   }
 }
