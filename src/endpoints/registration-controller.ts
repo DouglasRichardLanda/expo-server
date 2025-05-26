1399f04924de80795ad9613b56151b3321af4f72
Douglas Richard Landa <douglasrichardlanda@gmail.com>
2025-05-27 01:13:04 +0200

    task quee and second register endpoint

--- src/endpoints/registration-controller.ts
+++ src/endpoints/registration-controller.ts
@@ -1,7 +1,31 @@
+import type {Request as REQ, Response as RES} from "express";
 
+const codes: {code: string, createdAt: number, name: string, email: string}[] = [];
 
 class RegistrationController {
+  async register_step1 (req: REQ, res: RES) {
+    try {
+      const {name, email} = req.body;
 
+      const code = String(Math.floor(Math.random() * 9999) + 1).padStart(4, '0');
+      codes.push({code: code, createdAt: Date.now(), name: name, email: email})
+
+      res.status(202).json({success: true})
+    } catch (e) {
+      res.status(500).json({success: false})
+    }
+  }
+  async register_step2 (req: REQ, res: RES) {
+    try {
+      const {usercode} = req.body;
+
+      console.log("USERCODE: ", usercode)
+
+      res.status(202).json({success: true})
+    } catch (e) {
+      res.status(500).json({success: false})
+    }
+  }
 }
 
 export default new RegistrationController()
--- src/endpoints/registration-router.ts
+++ src/endpoints/registration-router.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import RegistrationController from "./registration-controller.ts";
 
 
 const RegistrationRouter: express.Router = express.Router()
 
+RegistrationRouter.post('/first', RegistrationController.register_step1)
 
 export default RegistrationRouter;
