f9cd14eed64e13b492d737f5c8c1cd3877d9b35f
Douglas Richard Landa <douglasrichardlanda@gmail.com>
2025-06-01 14:04:40 +0200

    added router

--- src/endpoints/data-router.ts
+++ src/endpoints/data-router.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import pool from "../bd-connect.ts";
+import date_number from "../helpers/date-number.ts";
+import name_number from "../helpers/name-number.ts";
+import calculate_lucky_number from "../helpers/lucky-number.ts";
 
 
 const DataRouter: express.Router = express.Router()
@@ -20,7 +23,13 @@ DataRouter.post('/changename', async (req: express.Request, res: express.Respons
   try {
     const {name, father, email} = req.body;
 
-    const [row]: any = await pool.query(`update users set name = ?, fathername = ? where email = ?`, [name, father, email])
+    const [row1]: any = await pool.query(`select * from users where email = ?`, [email])
+    const user = row1[0];
+
+    const new_nnumber = name_number(`${name} ${father}`)
+    const new_lnumber = calculate_lucky_number(user.lbnumber, new_nnumber)
+
+    const [row]: any = await pool.query(`update users set name = ?, fathername = ?, lnumber = ?, lnnumber = ? where email = ?`, [name, father, new_lnumber, new_nnumber, email])
 
     res.status(200).json({success: true})
   } catch (e) {
