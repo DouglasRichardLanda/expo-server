c829def2a390241c0a9eece83525c97aa523aaeb
Douglas Richard Landa <douglasrichardlanda@gmail.com>
2025-06-01 13:58:56 +0200

    updated fn for change name, not ready

--- src/endpoints/data-router.ts
+++ src/endpoints/data-router.ts
@@ -16,5 +16,16 @@ DataRouter.get('/userdata', async (req: express.Request, res: express.Response) 
     res.status(500).json({success: false})
   }
 })
+DataRouter.post('/changename', async (req: express.Request, res: express.Response) => {
+  try {
+    const {name, father, email} = req.body;
+
+    const [row]: any = await pool.query(`update users set name = ?, fathername = ? where email = ?`, [name, father, email])
+
+    res.status(200).json({success: true})
+  } catch (e) {
+    res.status(500).json({success: false})
+  }
+})
 
 export default DataRouter;
